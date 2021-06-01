e1fac29afe007293a41a6a06f3046a0a9ceec0bf
Damian Klek <damian.artur.klek@gmail.com>
2021-06-01 13:11:43 +0200

    Change order of properties

--- PropertyRental.Domain/Entities/Image.cs
+++ PropertyRental.Domain/Entities/Image.cs
@@ -4,9 +4,9 @@ namespace PropertyRental.Domain.Entities
 {
   public class Image : AuditableEntity
   {
-    public int PropertyId { get; set; }
-    public Property Property { get; set; }
     public string Name { get; set; }
     public string SourcePath { get; set; }
+    public int PropertyId { get; set; }
+    public Property Property { get; set; }
   }
 }
