71c1eb2493c8e1b900342537df6612759eb0c6fa
Damian Klek <damian.artur.klek@gmail.com>
2021-06-01 13:00:56 +0200

    Add placeholders for controllers

--- PropertyRental/Controllers/ImageController.cs
+++ PropertyRental/Controllers/ImageController.cs
@@ -1,13 +1,21 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MovieManagement.Api.Models;
+using System.Threading.Tasks;
 
 namespace PropertyRental.Controllers
 {
   [Route("api/images")]
+  [ApiController]
   public class ImageController : Controller
   {
-    public IActionResult Index()
+    [HttpGet]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [ProducesResponseType(StatusCodes.Status403Forbidden, Type = typeof(ErrorModel))]
+    public async Task<ActionResult<string>> GetImagesAsync()
     {
-      return View();
+      return "PlaceHolder";
     }
   }
 }
