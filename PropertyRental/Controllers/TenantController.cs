9b5ff125eb03c0683bbf1e2cabb301d4d1527072
Damian Klek <damian.artur.klek@gmail.com>
2021-06-01 13:00:56 +0200

    Add placeholders for controllers

--- PropertyRental/Controllers/TenantController.cs
+++ PropertyRental/Controllers/TenantController.cs
@@ -1,13 +1,21 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MovieManagement.Api.Models;
+using System.Threading.Tasks;
 
 namespace PropertyRental.Controllers
 {
   [Route("api/tenants")]
+  [ApiController]
   public class TenantController : Controller
   {
-    public IActionResult Index()
+    [HttpGet]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [ProducesResponseType(StatusCodes.Status403Forbidden, Type = typeof(ErrorModel))]
+    public async Task<ActionResult<string>> GetTenantsAsync()
     {
-      return View();
+      return "PlaceHolder";
     }
   }
 }
