d59a1db59ac24992dd711414b979fd99d8390b22
Damian Klek <damian.artur.klek@gmail.com>
2021-06-03 20:45:06 +0200

    Override SaveChangesAsync in DbContext

--- PropertyRental.Persistance/PropertyDbContext.cs
+++ PropertyRental.Persistance/PropertyDbContext.cs
@@ -1,4 +1,8 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using PropertyRental.Domain.Common;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace PropertyRental.Persistance
 {
@@ -7,6 +11,41 @@ namespace PropertyRental.Persistance
     public PropertyDbContext(DbContextOptions<PropertyDbContext> options) : base(options)
     {
 
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+
+    }
+
+    public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = new CancellationToken())
+    {
+      foreach (var entry in ChangeTracker.Entries<AuditableEntity>())
+      {
+        switch (entry.State)
+        {
+          case EntityState.Added:
+            entry.Entity.CreatedBy = string.Empty;
+            entry.Entity.Created = DateTime.Now;
+            entry.Entity.StatusId = 1;
+            break;
+          case EntityState.Modified:
+            entry.Entity.ModifiedBy = string.Empty;
+            entry.Entity.Modified = DateTime.Now;
+            entry.Entity.StatusId += 1;
+            break;
+          case EntityState.Deleted:
+            entry.Entity.ModifiedBy = string.Empty;
+            entry.Entity.Modified = DateTime.Now;
+            entry.Entity.InactivatedBy = string.Empty;
+            entry.Entity.Inactivated = DateTime.Now;
+            entry.Entity.StatusId = 0;
+            entry.State = EntityState.Modified;
+            break;
+        }
+      }
+
+      return base.SaveChangesAsync(cancellationToken);
     }
   }
 }
