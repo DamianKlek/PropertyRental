fd0da1408fdb3ca5c1a3ef792cfe3926025edc2b
Damian Klek <damian.artur.klek@gmail.com>
2021-06-04 11:16:10 +0200

    Add Availability entity and it's relation with PropertyRent

---
+++ PropertyRental.Domain/Entities/Availability.cs
@@ -0,0 +1,13 @@
+﻿using PropertyRental.Domain.Common;
+using System;
+
+namespace PropertyRental.Domain.Entities
+{
+  public class Availability : AuditableEntity
+  {
+    public DateTime Day { get; set; }
+    public int AvailabilityStatus { get; set; }
+    public int PropertyRentId { get; set; }
+    public PropertyRent PropertyRent { get; set; }
+  }
+}
