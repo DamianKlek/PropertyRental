01c06910ed7a53715e2fea9803870b7059e056c2
Damian Klek <damian.artur.klek@gmail.com>
2021-06-04 11:16:10 +0200

    Add Availability entity and it's relation with PropertyRent

--- PropertyRental.Domain/Entities/PropertyRent.cs
+++ PropertyRental.Domain/Entities/PropertyRent.cs
@@ -8,6 +8,7 @@ namespace PropertyRental.Domain.Entities
     public decimal DayCost { get; set; }
     public int PropertyId { get; set; }
     public Property Property { get; set; }
+    public ICollection<Availability> Availabilities { get; set; }
     public ICollection<Contract> Contracts { get; set; }
   }
 }
