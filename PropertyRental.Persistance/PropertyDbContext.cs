c411fb0bd9572e4d33df3ddd8decb09c5a26ae08
Damian Klek <damian.artur.klek@gmail.com>
2021-07-07 23:58:34 +0200

    Add DbSets to DdContext

--- PropertyRental.Persistance/PropertyDbContext.cs
+++ PropertyRental.Persistance/PropertyDbContext.cs
@@ -1,51 +1,62 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using PropertyRental.Domain.Common;
+using PropertyRental.Domain.Entities;
 using System;
 using System.Threading;
 using System.Threading.Tasks;
 
 namespace PropertyRental.Persistance
 {
-  public class PropertyDbContext : DbContext
-  {
-    public PropertyDbContext(DbContextOptions<PropertyDbContext> options) : base(options)
-    {
+	public class PropertyDbContext : DbContext
+	{
+		public PropertyDbContext(DbContextOptions<PropertyDbContext> options) : base(options)
+		{
 
-    }
+		}
 
-    protected override void OnModelCreating(ModelBuilder modelBuilder)
-    {
+		public DbSet<Availability> Availabilities { get; set; }
+		public DbSet<Contract> Contracts { get; set; }
+		public DbSet<Image> Images { get; set; }
+		public DbSet<Property> Properties { get; set; }
+		public DbSet<PropertyAddress> PropertyAddresses { get; set; }
+		public DbSet<PropertyRent> PropertyRents { get; set; }
+		public DbSet<Tag> Tags { get; set; }
+		public DbSet<Tenant> Tenants { get; set; }
 
-    }
 
-    public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = new CancellationToken())
-    {
-      foreach (var entry in ChangeTracker.Entries<AuditableEntity>())
-      {
-        switch (entry.State)
-        {
-          case EntityState.Added:
-            entry.Entity.CreatedBy = string.Empty;
-            entry.Entity.Created = DateTime.Now;
-            entry.Entity.StatusId = 1;
-            break;
-          case EntityState.Modified:
-            entry.Entity.ModifiedBy = string.Empty;
-            entry.Entity.Modified = DateTime.Now;
-            entry.Entity.StatusId += 1;
-            break;
-          case EntityState.Deleted:
-            entry.Entity.ModifiedBy = string.Empty;
-            entry.Entity.Modified = DateTime.Now;
-            entry.Entity.InactivatedBy = string.Empty;
-            entry.Entity.Inactivated = DateTime.Now;
-            entry.Entity.StatusId = 0;
-            entry.State = EntityState.Modified;
-            break;
-        }
-      }
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
 
-      return base.SaveChangesAsync(cancellationToken);
-    }
-  }
+		}
+
+		public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = new CancellationToken())
+		{
+			foreach (var entry in ChangeTracker.Entries<AuditableEntity>())
+			{
+				switch (entry.State)
+				{
+					case EntityState.Added:
+						entry.Entity.CreatedBy = string.Empty;
+						entry.Entity.Created = DateTime.Now;
+						entry.Entity.StatusId = 1;
+						break;
+					case EntityState.Modified:
+						entry.Entity.ModifiedBy = string.Empty;
+						entry.Entity.Modified = DateTime.Now;
+						entry.Entity.StatusId += 1;
+						break;
+					case EntityState.Deleted:
+						entry.Entity.ModifiedBy = string.Empty;
+						entry.Entity.Modified = DateTime.Now;
+						entry.Entity.InactivatedBy = string.Empty;
+						entry.Entity.Inactivated = DateTime.Now;
+						entry.Entity.StatusId = 0;
+						entry.State = EntityState.Modified;
+						break;
+				}
+			}
+
+			return base.SaveChangesAsync(cancellationToken);
+		}
+	}
 }
