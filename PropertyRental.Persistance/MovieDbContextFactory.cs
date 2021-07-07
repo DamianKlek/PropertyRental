1889697a34e709cdc0540d8c2dcd8b1ddbe74a81
Damian Klek <damian.artur.klek@gmail.com>
2021-07-08 00:51:59 +0200

    Add MovieDbContextFactory class and it's implementation

---
+++ PropertyRental.Persistance/MovieDbContextFactory.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MovieManagement.Persistance;
+
+namespace PropertyRental.Persistance
+{
+	public class MovieDbContextFactory : DesignTimeDbContextFactoryBase<PropertyDbContext>
+	{
+		protected override PropertyDbContext CreateNewInstance(DbContextOptions<PropertyDbContext> options)
+		{
+			return new PropertyDbContext(options);
+		}
+	}
+}
