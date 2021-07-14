5e4da77c691543acf7e93c2c8ca42afe4df53be3
Damian Klek <damian.artur.klek@gmail.com>
2021-07-14 22:24:05 +0200

    Add exchange rate external API

---
+++ PropertyRental.Application/Common/Interfaces/INBPClient.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace PropertyRental.Application.Common.Interfaces
+{
+	public interface INBPClient
+	{
+		Task<string> GetExchangeRate(char tableName, string searchFilter, CancellationToken cancellationToken);
+	}
+}
--- PropertyRental.Infrastructure/DependencyInjection.cs
+++ PropertyRental.Infrastructure/DependencyInjection.cs
@@ -1,8 +1,11 @@
 ﻿using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using PropertyRental.Application.Common.Interfaces;
+using PropertyRental.Infrastructure.ExternalAPIs.NBPExchangeRate;
 using PropertyRental.Infrastructure.FileStore;
 using PropertyRental.Infrastructure.Services;
+using System;
+using System.Net.Http;
 
 namespace PropertyRental.Infrastructure
 {
@@ -10,6 +13,15 @@ namespace PropertyRental.Infrastructure
 	{
 		public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
 		{
+			services.AddHttpClient("NBPClient", options =>
+			{
+				options.BaseAddress = new Uri("http://api.nbp.pl");
+				options.Timeout = new TimeSpan(0, 0, 10);
+				options.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+			}).ConfigurePrimaryHttpMessageHandler(sp => new HttpClientHandler());
+
+			services.AddScoped<INBPClient, NBPClient>();
+
 			services.AddTransient<IDateTime, DateTimeService>();
 			services.AddTransient<IFileStore, FileStore.FileStore>();
 			services.AddTransient<IFileWrapper, FileWrapper>();
