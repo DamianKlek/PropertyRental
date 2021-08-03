e68b2cb696a29dabec265710b7727e0f76b3073a
DamianKlek <damian.artur.klek@gmail.com>
2021-08-03 21:16:10 +0200

    Fix missing Id property in UpdateProperty command

--- PropertyRental.Application/Common/Properties/Commands/UpdateProperty/UpdatePropertyCommand.cs
+++ PropertyRental.Application/Common/Properties/Commands/UpdateProperty/UpdatePropertyCommand.cs
@@ -7,6 +7,7 @@ namespace PropertyRental.Application.Common.Properties.Commands.UpdateProperty
 {
 	public class UpdatePropertyCommand : IRequest, IMapFrom<Property>
 	{
+		public int Id { get; set; }
 		public string Name { get; set; }
 		public string Description { get; set; }
 
